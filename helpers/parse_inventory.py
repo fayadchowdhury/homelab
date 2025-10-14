018a8d2bdbe15bd09b4ad7ac2f155842107f7468
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-10-14 15:00:46 -0700

    Write a helper script to parse Terraform provisioned nodes into Ansible inventory

---
+++ helpers/parse_inventory.py
@@ -0,0 +1,39 @@
+from typing import Dict
+import sys
+import json
+
+def parse_tf_output(json_string: str) -> Dict:
+    return json.loads(json_string)["vm_metadata"]["value"]
+
+def generate_inventory_dict(vm_metadata: Dict) -> Dict:
+    inventory = {}
+    for k, v in vm_metadata.items():
+        if "master" in k:
+            print(f"Master node found: {k}")
+            if "kube-master" not in inventory:
+                inventory["kube-master"] = []
+            inventory["kube-master"].append(v["ip"].split("/")[0])
+        if "worker" in k:
+            print(f"Worker node found: {k}")
+            if "kube-worker" not in inventory:
+                inventory["kube-worker"] = []
+            inventory["kube-worker"].append(v["ip"].split("/")[0])
+    return inventory
+
+def write_inventory_file(inventory: Dict, path: str) -> None:
+    with open(path, "w") as f:
+        for k, v in inventory.items():
+            f.write(f"[{k}]\n")
+            for i in v:
+                f.write(f"{i}\n")
+            f.write(f"\n")
+    return
+
+if __name__=="__main__":
+    json_string = sys.stdin.read()
+    vm_metadata = parse_tf_output(json_string)
+    inventory_dict = generate_inventory_dict(vm_metadata)
+    inventory_file_output = sys.argv[1]
+    write_inventory_file(inventory_dict, inventory_file_output)
+
+    print(f"Complete inventory:\n{inventory_dict}")
