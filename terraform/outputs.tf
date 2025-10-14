48867f5b9586e7008533eea6209b011aefd79d89
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-10-13 22:55:22 -0700

    Output VM name and IP address from Terraform

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "vm_metadata" {
+    value = {
+        for k, vm in proxmox_vm_qemu.vms :
+        k => {
+            ip   = regex("ip=([^,]+)", vm.ipconfig0)[0]
+            name = vm.name
+        }
+    }
+}
